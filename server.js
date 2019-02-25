10e16daaa1495749eb61c3046f049a88ee34c988
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-02-25 07:15:21 -0600

    fixed handlebars error

--- server.js
+++ server.js
@@ -2,11 +2,10 @@ var express = require("express");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var exphbs = require("express-handlebars");
+var app = express();
 
-// app.engine("handlebars", exphbs({
-//   defaultLayout: "main"
-// }));
-// app.set("view engine", "handlebars");
+app.engine("handlebars", exphbs({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 
 var axios = require("axios");
 var cheerio = require("cheerio");
@@ -14,7 +13,6 @@ var cheerio = require("cheerio");
 var db = require("./models");
 
 var PORT = 3000;
-var app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -23,7 +21,9 @@ app.use(express.static("public"));
 mongoose.connect("mongodb://localhost/HeadlinesRUs", { useNewUrlParser: true });
 
 //ROUTES
-
+app.get("/", function(request, response) {
+    response.render("article");
+})
 app.get("/scrape", function ( request, response) {
     axios.get("https://www.cnn.com/world").then(function(response){
         var $ = cheerio.load(response.data);
