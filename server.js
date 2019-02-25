0b59937bae3f3d1a7cbc396fdc06ad5fe95ce55a
kjojones <43149867+kfroerer@users.noreply.github.com>
2019-02-25 07:07:25 -0600

    now adding all scraped to db

--- server.js
+++ server.js
@@ -32,19 +32,58 @@ app.get("/scrape", function ( request, response) {
         $("article").each(function(i, element) {
             results.title = $(this).find("h3").find("span").text();
             results.link = $(this).find("a").attr("href");
+           
             console.log(results);
         })
-         
-        db.Article.create(results)
-        .then(function(dbArticle) {
-          console.log(dbArticle);
+        //need to validate and check for duplicates
+        db.Article.insertMany(results)
+        .then(function(docs) {
+          console.log(docs);
         })
         .catch(function(err) {
           console.log(err);
         });
     })
     response.send("Scrape Complete");
+});
+
+//route for getting all the articles from db
+app.get("/articles", function (request, response ){ 
+    db.Article.find({})
+    .then(function(result){
+        response.json(result);
+    })
+    .catch(function(err) {
+        response.json(err)
+    });
+});
+
+//returns specific article with comment
+app.get("/articles/:id", function (request, response) {
+    db.Article.findOne({_id: request.params.id})
+    .populate("comment")
+    .then(function(result) {
+        response.json(result)
+    })
+    .catch(function(err){
+        response.json(err)
+    });
+});
+
+//save an article's commment
+app.post("/articles/:id", function(request, response) {
+    db.Comment.create(request.body)
+    .then(function(newComment){
+        return db.Article.findOneAndUpdate({_id: request.params.id}, {comment: newComment._id}, {new: true});
+    })
+    .then(function(updatedArticle){
+        response.json(updatedArticle);
+    })
+    .catch(function(err){
+        response.json(err);
+    });
 })
+
 
 app.listen(PORT, function() {
     console.log("App running on port " + PORT + "!");
